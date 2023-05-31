4588684521ca0fd469b7f50fc43e57e9de1ff7c8
tannisthanandy <96834486+tannisthanandy@users.noreply.github.com>
2023-06-01 01:22:49 +0530

    Add files via upload

---
+++ ffdfdd.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+const baseUrl = '/calculator'
+
+app.use(express.json());
+
+const baseRouter = express.Router();
+
+baseRouter.get('/greeting', (req, res) => {
+    return res.status(200).send('Hello world!');
+  });
+  
+  baseRouter.post('/add', (req, res) => {
+      var {first,second} = req.body;
+      res.status(200).json({ "result":  first+second});
+  });
+  
+  
+  baseRouter.post('/subtract', (req, res) => {
+      var {first,second} = req.body;
+      res.json({ "result": first-second });
+  });
+
+app.use(baseUrl, baseRouter);
+app.listen(PORT, () => {
+    console.log("Server running at PORT", PORT);
+});
